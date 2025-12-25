b3aeba412e41a88cabe8bca1842da59f439484c6
Thanadon.aon <aon59079@gmail.com>
2025-12-25 10:56:58 +0700

    add func-components
    add func-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "./style.css"
+import React from 'react';
+import { Header } from './func-components';
+import { Content } from './func-components';
+import { Footer } from './func-components';
 function App() {
   return (
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className="title">React & React Native</div>
-    </>
+    [<Header/>,<Content/>,<Footer/>]
   )
 }
-
 export default App;
