d04eccc05051a170bb0e33521494fdc79a2c61a5
Thanadon.aon <aon59079@gmail.com>
2026-02-12 10:05:08 +0700

    navlink in router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
+import Router2 from './router2';
 
-import Content from './context-content';
-import { UserContext } from './context';
-import Content2 from './context-content2';
-import Header2 from './context-header2';
-function App() {
-  let [user,setUser]=React.useState(null)
-  return (
-    <UserContext.Provider value={[user,setUser]}>
-      <Header2 />
-      <Content2 />
-    </UserContext.Provider>
-  )
+export default function App() {
+  return <Router2 />;
 }   
-export default App;
--- src/router2.js
+++ src/router2.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Link, NavLink } from "react-router-dom";
-import './NavLink.css';
+import { BrowserRouter,NavLink } from "react-router-dom";
+import './navlink.css';
 
 export default function Router2() {
     return (
         <BrowserRouter>
             <nav className="nav">
-                <NavLink to='/' className={({isActive}) => isActive ? "active_menu" : "menu"}> style={({isActive})} => {
+                <NavLink to='/' className={({ isActive }) => isActive ? "active_menu" : "menu"} style={({ isActive }) => {
                     return {
-                        fontWeight: isActive ? "bold" : "",
+                        fontWeight: isActive ? "bold" : ""
                     };
-                }}>Home</NavLink> -&nbsp;
-                <NavLink to='/products' className={({isActive}) => isActive ? "active_menu" : "menu"}>Products</NavLink> -&nbsp;
-                <NavLink to='/member' className={({isActive}) => isActive ? "active_menu" : "menu"}>Member</NavLink> -&nbsp;
-                <NavLink to='/contact' className={({isActive}) => isActive ? "active_menu" : "menu"}>Contact Us</NavLink>
+                }}>Home </NavLink> -&nbsp;
+                <NavLink to='/products' className={({ isActive }) => isActive ? "active_menu" : "menu"}>Products</NavLink> -&nbsp;
+                <NavLink to='/member' className={({ isActive }) => isActive ? "active_menu" : "menu"}>Member</NavLink> -&nbsp;
+                <NavLink to='/contact' className={({ isActive }) => isActive ? "active_menu" : "menu"}>Contact Us</NavLink>
             </nav>
         </BrowserRouter>
     )
